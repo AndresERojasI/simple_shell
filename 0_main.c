99f96a41a03362f703a02af3258f0cd76f5374f5
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-04-10 21:09:17 +0000

    Added basic structure for compiling and CMake configuration

---
+++ 0_main.c
@@ -0,0 +1,12 @@
+#include "headers/header.h"
+#include <stdio.h>
+
+void simple_shell()
+{
+	printf("Simple shell - example");
+}
+
+int main(void){
+	simple_shell();
+	return (0);
+}
---
+++ headers/header.h
@@ -0,0 +1,26 @@
+/**H********************************************************************************
+ * Filename: main.c
+ *
+ * Description: This is the main starting point for the Simple Shell script
+ *
+ * Functions:
+ *
+ * Notes:
+ * 		This script is part of the Holberton School project "0x16. C - Simple Shell".
+ * 		This is simply an example of one way to build this project, and serve as
+ * 		reference point for current and future students, if you feel the need to copy
+ * 		this project and cheat, you have not understood the real values of Holberton
+ * 		and what a good Software Engineer is supposed to be.
+ *
+ * Author: Andrés Rojas (andres.rojas[@]holbertonschool.com)
+ * Start Date: April 10, 2020
+ *
+ * Change Log:
+ * 		- (April 10, 2020) Initial Structure and basic configuration (Not stable)
+ *
+ *H*/
+
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+
+#endif
